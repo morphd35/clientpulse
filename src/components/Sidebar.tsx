46a4e56c9b076e66cc591e91e0de3e62f011ab5b
morphd35 <89456062+morphd35@users.noreply.github.com>
2025-01-03 14:12:43 -0600

    Update Title from Southern Glazer to ClientPulse

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -60,7 +60,7 @@ export default function Sidebar({ open, setOpen }: SidebarProps) {
               <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
                   <div className="flex h-16 shrink-0 items-center">
-                    <h1 className="text-xl font-bold">Southern Glazer Sales</h1>
+                    <h1 className="text-xl font-bold">ClientPulse</h1>
                   </div>
                   <nav className="flex flex-1 flex-col">
                     <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -106,7 +106,7 @@ export default function Sidebar({ open, setOpen }: SidebarProps) {
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
-            <h1 className="text-xl font-bold">Southern Glazer Sales</h1>
+            <h1 className="text-xl font-bold">ClientPulse</h1>
           </div>
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
--- src/features/auth/components/AuthLayout.tsx
+++ src/features/auth/components/AuthLayout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({ children }: PropsWithChildren) {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
-          <h2 className="text-3xl font-bold text-gray-900">Southern Glazer Sales</h2>
+          <h2 className="text-3xl font-bold text-gray-900">ClientPulse</h2>
         </div>
         {children}
       </div>
